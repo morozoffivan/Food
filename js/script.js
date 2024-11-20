2498d63ebef8796337edca9f1856a56c992529ff
Morozov Ivan <morozoff.one@yandex.ru>
2024-11-20 15:11:07 +0300

    added tabs toggler

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hidden');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) =>{
+        const target = e.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
+});
